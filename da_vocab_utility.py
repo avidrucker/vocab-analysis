23947658385cea614c403be1432219af742de06a
Avi Drucker <avidrucker@users.noreply.github.com>
2019-05-01 17:12:41 -0400

    extract utility functions to external module

---
+++ da_vocab_utility.py
@@ -0,0 +1,60 @@
+# TDD backbone assertion to confirm a function call returns the desired result
+def assertEquals(actual, expected, desc):
+    assert(actual==expected), desc + " result: " + str(actual) + ", expected: " + str(expected)
+    return "OK"
+	
+# check that two lists have the same contents
+def lists_equal(a,b):
+    return (a == b).all()
+	
+
+# shallow check (by row) for duplicates
+def has_dupes(df_in):
+    dupe = df_in.duplicated()
+    return df_in.loc[dupe].shape[0] != 0
+	
+def print_line_break():
+    print("-"*75)
+	
+def print_before_after(b, a, t=""):
+    if t != "":
+        print_line_break()
+        print(t)
+    print_line_break()
+    print("Before: " + str(b))
+    print_line_break()
+    print("After: " + str(a))
+    print_line_break()
+	
+def time_it(func, *args, **kwargs):
+    start = time.time()
+    func(*args, **kwargs)
+    end = time.time()
+    # https://stackoverflow.com/questions/8885663/how-to-format-a-floating-number-to-fixed-width-in-python
+    print("{:.0f}".format((end - start)*1000) + " miliseconds")
+	
+def has_dupe_terms(df_in):
+    location = df_in['Term'].duplicated()
+    return df_in.loc[location].shape[0] != 0
+	
+def inspect_note(df_in, term):
+    return df_in[df_in['Term']==term]
+	
+def inspect_note_by_id(df_in, nid):
+    return df_in[df_in['nid']==nid]
+	
+# Converts a tag string to a list to a set back to a string (this removes the duplicates)
+def remove_dupes(t):
+    temp = list(set(t.lower().split()))
+    return ' '.join(temp) # return as string
+	
+# determines if an individual tag substring exists in a larger tags list string
+def tag_exists(tags, tag):
+    return 1 if tag in tags.split() else 0
+	
+def is_blank (s):
+    return not (s and s.strip())
+	
+def get_frame_of_cards_by_term(df, t):
+    return df.loc[df['Term']==t]
+	
